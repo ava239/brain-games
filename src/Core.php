d546da5f573b3d0ce8e09e37169cd4cf7de8c37e
ava <ava239@yandex.ru>
2020-07-01 21:26:45 +0300

    core refactoring

--- src/Core.php
+++ src/Core.php
@@ -5,42 +5,30 @@ namespace Brain\Games\Core;
 use function cli\line;
 use function cli\prompt;
 
-function startGameFlow($rules, $generateFunction)
+const ROUNDS_COUNT = 3;
+
+function startGameFlow($rules, $gameDataFunction)
 {
     line('Welcome to the Brain Game!');
     line($rules);
-    line('');
+    line();
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    $question = $correct = 0;
-    $maxQuestions = 3;
-    while ($question < $maxQuestions && $question === $correct) {
-        $question++;
-        if (playOneGameRound($generateFunction)) {
-            $correct++;
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
+        [$questionText, $answerText] = $gameDataFunction();
+        line('Question: %s', $questionText);
+        $answer = prompt('Your answer');
+        if ($answer === $answerText) {
+            line('Correct!');
+        } else {
+            line(
+                "'%s' is wrong answer ;(. Correct answer was '%s'.",
+                $answer,
+                $answerText
+            );
+            line("Let's try again, %s!", $name);
+            return;
         }
     }
-    if ($question === $correct) {
-        line("Congratulations, %s!", $name);
-    } else {
-        line("Let's try again, %s!", $name);
-    }
-}
-
-function playOneGameRound($generateFunction)
-{
-    [$questionText, $answerText] = $generateFunction();
-    line('Question: %s', $questionText);
-    $answer = prompt('Your answer');
-    if ($answer === $answerText) {
-        line('Correct!');
-        return true;
-    } else {
-        line(
-            "'%s' is wrong answer ;(. Correct answer was '%s'.",
-            $answer,
-            $answerText
-        );
-        return false;
-    }
+    line("Congratulations, %s!", $name);
 }
